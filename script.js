7e7ccf087e8e4c4ab995b4fd56c62c8a6cbf468c
zzhhaa <zzhhaa@protonmail.com>
2023-11-13 09:28:09 -0800

    Add responsive CSS for mobile

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ let factgen = document.getElementById("random_fact");
 let factout = document.getElementById("fact_output");
 let facts = [
   "I make electronic music. My first exposure to programming was through exploring the visual pseudo-code language Max/MSP, which lets you build instruments, effects, and lots more. That was 10 years ago and I still don’t know how to use it!",
-  "I’m a permaculture enthusiast! ‘Permaculture’ is a concatenation of ‘permanent’ and ‘agriculture’. Some say permaculture has as many definitions as practitioners. I’ve heard it referred to as garden automation. Personally, I think of it as a a way of bringing systems-thinking into growing and place design. Ethics are central to permaculture; its key principles are <em>people-care</em>, <em>earth-care</em>, and <em>fair-share</em>.", 
-  "My brain is a Notion database. This is hyperbole, but I do use the software to organise just about everything – even if I don’t like that it a.) doesn’t allow offline storage, b.) is hosted on AWS. I’m particularly enjoying using the database feature to put together my own searchable code glossary, to which I add all the functions, methods, etc. that I’ve encountered to date.", 
+  "I’m a permaculture enthusiast! ‘Permaculture’ concatenates ‘permanent’ and ‘agriculture’; some say it has as many definitions as practitioners. Personally, I think of it as applying systems-thinking in growing. Ethical values of <em>people-care</em>, <em>earth-care</em>, and <em>fair-share</em> are central.", 
+  "My brain is a Notion database. This is hyperbole, but I do use the software to organise just about everything. I’m particularly enjoying using the database feature to put together my own searchable code glossary, to which I add all the functions, methods, etc. that I’ve encountered to date.", 
   "I have a wormery and use it to make compost. My worms are fed a steady diet of coffee grounds and banana peels, as well as scrap paper and cardboard. They served as the inspiration for my game!", 
   "Orange is one of my favourite colours. Can you tell? (Try highlighting some text!)"
 ];
